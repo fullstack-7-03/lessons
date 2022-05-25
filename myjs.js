1e5fd4f4576b18841d5fad384ca7efd94bdded00
Alex Gorbachov <alexpitronot@gmail.com>
2022-05-25 17:31:22 +0300

    L12

---
+++ myjs.js
@@ -0,0 +1,17 @@
+function open_text() {
+    if (document.getElementById("secret").style.display == '') {
+        document.getElementById("secret").style.display = 'flex'
+        document.getElementById('my').innerHTML = 'סגור קוד סודי'
+    }
+    else {
+        document.getElementById("secret").style.display = ''
+        document.getElementById('my').innerHTML = 'פתח קוד סודי'
+    }
+} 
+
+function colorbg() {
+    if(document.body.style.backgroundColor=="plum")
+    document.body.style.backgroundColor="tomato";
+    else
+    document.body.style.backgroundColor="plum";
+}
