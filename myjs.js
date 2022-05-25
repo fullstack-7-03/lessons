baa79cd00e2e3316effd852767b583074ae63ce3
alexpitronot <alexpitronot@gmail.com>
2022-05-25 19:59:43 +0300

    L12-class

--- myjs.js
+++ myjs.js
@@ -7,11 +7,16 @@ function open_text() {
         document.getElementById("secret").style.display = ''
         document.getElementById('my').innerHTML = 'פתח קוד סודי'
     }
-} 
+}
 
 function colorbg() {
-    if(document.body.style.backgroundColor=="plum")
-    document.body.style.backgroundColor="tomato";
+    if (document.body.style.backgroundColor == "plum" || document.body.style.backgroundColor == "black")
+        document.body.style.backgroundColor = "tomato";
     else
-    document.body.style.backgroundColor="plum";
+        document.body.style.backgroundColor = "black";
+}
+
+function changekishur() {
+    document.getElementById("kishur").href = "http://www.ynet.co.il";
+    document.getElementById("kishur").innerText = "YNET"
 }
